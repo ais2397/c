33da428d123b00a73e1fbea8f5faa2717c2e3077
ais2397 <35501599+ais2397@users.noreply.github.com>
2018-03-12 02:56:53 +0530

    Update Caesarcipher(envvariables).c

--- Caesarcipher(envvariables).c
+++ Caesarcipher(envvariables).c
@@ -4,18 +4,25 @@
 
 int main(int argc, char *argv[])
 {
-int n,x=26,j;
-int * z;
+int n,x=26,j,i;
 char * s[1000];
 char y[1000];
-if (argc==2)
+const char *name="TEST"; 
+char* z=getenv(name);
+i=atoi(z);
+
+
+if (argc!=1)
 	{
 	strcpy(s,argv[1]);
+	if (i!=NULL)
+	{
+	(n=i);
+	}
+	else
+	{
 	n=13;
 	}
-else if (argc>=3)
-	{strcpy(s,argv[1]);
-	n= (atoi(argv[2]));
 	}
 strcpy(y,s);
 for (j=0;j<strlen(y);j++)
@@ -29,7 +36,8 @@ for (j=0;j<strlen(y);j++)
 	{
 	y[j]=y[j]-x;
 	}
-	}
+}
 printf("The encoded text is: %s", y);
+printf("%d",i);
 return 0;
 }
